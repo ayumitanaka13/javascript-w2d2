070ebca0ab62a16c46c6ba8270c395e30ae621db
ayumitanaka <tanakayumi13@gmail.com>
2021-01-12 16:24:48 -0800

    modified assignment

--- ToDoList/todos.js
+++ ToDoList/todos.js
@@ -4,31 +4,32 @@ const todos = ['Collect Chicken Eggs','Clean Litter Box'];
 
 //start with a while loop that checks for quit input
 while(input !== 'q'){
-    input = prompt('What would you like to do?');
-
+    
     //if else conditions
-    if (input == 'new') {
+    if (input === 'new') {
         let task = prompt('What do you want to add to Todos?');
         todos.push(task);
         console.log('Added successfully');
-    } else if (input == 'list') {
+    } else if (input === 'list') {
         console.log('Here are all todos');
         for (let i = 0; i < todos.length; i++) {
             console.log(`${i}: ${todos[i]}`);
         }
-    } else if (input == 'delete') {
-        let number = prompt('Which number of todos do you want to delete?');
+    } else if (input === 'delete') {
+        let number = prompt('Which index of todos do you want to delete?');
         if (number >= todos.length || number < 0) {
             console.log('Wrong input');
         } else {
             todos.splice(number, 1);
             console.log('Deleted successfully');
         }
-    } else if (input == 'q') {
+    } else if (input === 'q') {
         break;
     } else {
         console.log('try again');
     }
+
+    input = prompt('What would you like to do?');
 }
 
 console.log('Bye Bye');
